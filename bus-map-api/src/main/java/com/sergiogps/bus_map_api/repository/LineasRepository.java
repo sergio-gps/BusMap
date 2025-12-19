5d83e9a6aa586035f94890d2d35d255f1d0f3e73
Sergio <>
2025-12-19 11:52:25 +0100

    Creación del repositorio que faltaba y cambios en Lineas y Localización

--- bus-map-api/src/main/java/com/sergiogps/bus_map_api/repository/LineasRepository.java
+++ bus-map-api/src/main/java/com/sergiogps/bus_map_api/repository/LineasRepository.java
@@ -1,7 +1,9 @@
 package com.sergiogps.bus_map_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sergiogps.bus_map_api.entity.Lineas;
 
+@Repository
 public interface LineasRepository extends JpaRepository<Lineas, Integer> { }
--- bus-map-api/src/main/java/com/sergiogps/bus_map_api/repository/ParadasRepository.java
+++ bus-map-api/src/main/java/com/sergiogps/bus_map_api/repository/ParadasRepository.java
@@ -3,7 +3,7 @@ package com.sergiogps.bus_map_api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sergiogps.bus_map_api.entity.Localizacion;
+import com.sergiogps.bus_map_api.entity.Paradas;
 
 @Repository
-public interface LocalizacionRepository extends JpaRepository<Localizacion, Integer> { }
+public interface ParadasRepository extends JpaRepository<Paradas, Integer> {}
